789227b3aa50507e6253e0370099c8de1c78fd7c
“pinki <pinkijha1310@gmail.com>
2023-12-26 12:14:15 +0530

    basic looping print reverse number from 10 to 1

--- 02-JS-fundamentals/loop/script.js
+++ 02-JS-fundamentals/loop/script.js
@@ -1,5 +1,13 @@
 // *****Basic Looping: Write a for loop that prints numbers from 1 to 10 *****
 
-for (let i = 1; i <= 10; i++ ){
+/* for (let i = 1; i <= 10; i++ ){
     console.log(i);
+} */
+
+
+
+// Reverse Looping: Write a for loop that prints numbers from 10 to 1.
+
+for ( let rev = 10; rev >= 1; rev--){
+    console.log(rev);
 }
