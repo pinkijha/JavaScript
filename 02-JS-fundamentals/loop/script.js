8a90da0836c2457abad45a1e1cd6667d06d399e5
“pinki <pinkijha1310@gmail.com>
2023-12-26 12:19:55 +0530

    basic looping print even number from 2 to 20

--- 02-JS-fundamentals/loop/script.js
+++ 02-JS-fundamentals/loop/script.js
@@ -8,6 +8,16 @@
 
 // Reverse Looping: Write a for loop that prints numbers from 10 to 1.
 
-for ( let rev = 10; rev >= 1; rev--){
+/* for ( let rev = 10; rev >= 1; rev--){
     console.log(rev);
+} */
+
+
+
+// Even Numbers: Write a for loop that prints all even numbers between 1 and 20.
+
+for (let even = 2; even <=20; even++){
+    if (even % 2 == 0){
+        console.log(`even number from 2 to 20:  ${even}`);
+    }
 }
