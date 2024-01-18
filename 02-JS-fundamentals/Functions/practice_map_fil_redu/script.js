ac9cb425ed5e19d75d82c56bd26da660ab385c30
“pinki <pinkijha1310@gmail.com>
2024-01-18 19:33:53 +0530

    JS Practice

--- 02-JS-fundamentals/Functions/practice_map_fil_redu/script.js
+++ 02-JS-fundamentals/Functions/practice_map_fil_redu/script.js
@@ -5,7 +5,7 @@ const user = [
     // list of fullname 
     // [pinki, jha]
 
-    let fullName = user.map((element) => console.log(`${element.firstName} ${element.lastName}`));
+    // let fullName = user.map((element) => console.log(`${element.firstName} ${element.lastName}`));
 
 
     // find the num of same age
@@ -20,12 +20,12 @@ const user = [
     return acc;
 
     }, {});
-    console.log(sameAge);
+    // console.log(sameAge);
 
 
     // find the name whose age is less than 30
     let output = user.filter(x => x.age < 30).map(x => x.firstName);
-    console.log(output);
+    // console.log(output);
 
 
     
@@ -35,6 +35,39 @@ const user = [
         return (curr.age < 30 && !acc) ? curr.name : acc;  
     });
     
-    console.log(name);
+    // console.log(name);
+
+    let result = 2009 ;
+    const func = (x = 262) => {
+        result = x;
+        // console.log(result);
+    }
+    func(null);
+
+    result = 0 || 1 && 2 || 3;
+    // console.log(result);
+
+    const plus = +0;
+    const minus = -0;
+    result = plus === minus;
+    // console.log(result);
+
+    'use strict';
+    result = "1" == 1;
+    // console.log(result);
+
+    let value = new Set([1, 1, 2, 3, 2, 3]);
+    result = [...value];
+    // console.log(result);
+
+    const getA = () => 1;
+    const getB = () => 2;
+    const getC = () => 3;
+    result = (getA(), getB(), getC());
+    // console.log(result);
+
+    const array =[ 10, 15, 20, 30, 40];
+    result = array.find( v => v > 15);
+    // console.log(result);
 
     
