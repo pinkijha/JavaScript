4ea4907a96df9bd87fbc26d40e6d539f808ee12b
“pinki <pinkijha1310@gmail.com>
2024-01-23 10:47:50 +0530

    practice questions     we are give array of marks of students . filter our of the marks of students that scored 90

--- 02-JS-fundamentals/Functions/script.js
+++ 02-JS-fundamentals/Functions/script.js
@@ -21,15 +21,30 @@ console.log(`Number of vowel in "${inputString}" : `, findVowel(inputString)); *
 const countVowel = (str) => {
     str = str.toLowerCase();
     let count = 0;
-    for ( let i = 0; i < str.length; i++){
-       if ( str[i] === 'a'|| str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u' ){
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
 
-          
-           count++;
 
-       }
+            count++;
+
+        }
     }
     return count;
 }
 const inputString = "RINKU JHA";
-console.log(`Number of vowel in "${inputString}" : `, countVowel(inputString));
+// console.log(`Number of vowel in "${inputString}" : `, countVowel(inputString));
+
+
+
+/*  practice questions 
+    we are give array of marks of students . filter our of the marks of students that scored 90 */
+
+let studentScore = [{ name: 'pinki', surname: 'jha', marks: 99 },
+                    { name: 'shailesh', surname: 'thakur', marks: 89 },
+                    { name: 'swati', surname: 'kadam', marks: 79 },
+                    { name: 'santoshi', surname: 'jha', marks: 98 },
+                    { name: 'sakshi', surname: 'jha', marks: 93 },
+];
+
+let newScore = studentScore.filter( item => item.marks > 90);
+console.log(newScore);
