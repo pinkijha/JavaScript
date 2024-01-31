10fdde2242c5e19b749be7c2343c2411cdce5bfe
“pinki <pinkijha1310@gmail.com>
2024-01-31 21:48:32 +0530

    bulb on/off using normal function & Arrow Function

--- 02-JS-fundamentals/Events/practice/practice/script.js
+++ 02-JS-fundamentals/Events/practice/practice/script.js
@@ -1,4 +1,4 @@
-let modebtn = document.querySelector("#mode");
+/* let modebtn = document.querySelector("#mode");
 
 let currMode = 'light'; //dark
 modebtn.addEventListener("click", () => {
@@ -10,6 +10,62 @@ modebtn.addEventListener("click", () => {
         currMode = 'light';
         document.querySelector('body').style.backgroundColor = 'white';
     }
-    console.log(currMode);
+    // console.log(currMode);
 
+}) */
+
+// light on|off
+/* 
+let light = document.querySelector("#mode");
+let switchs = 'on';
+
+light.addEventListener("click", () => {
+
+    let image = document.createElement("img");
+    // image.innerHTML = "<img src='pic_bulboff.gif'>";
+    image.setAttribute("src", "pic_bulboff.gif")
+    image.appendChild(document.createTextNode(""))
+
+
+
+    if( switchs === 'on') {
+        // switchs = 'off'
+
+
+    }
+    else{
+        switchs = 'on';
+    }
+    console.log(switchs)
+}) */
+
+
+
+
+// click on img for light on and off
+let image = document.querySelector("#img");
+
+image.addEventListener("click", () =>
+{
+    if (image.src.match('bulbon')){
+        image.src = "pic_bulboff.gif";
+
+    }
+    else{
+        image.src = "pic_bulbon.gif";
+    }
 })
+
+//using normal function
+function changeImage(){
+    image = document.getElementById('img');
+    if (image.src.match('bulbon')){
+        image.src = "pic_bulboff.gif";
+
+    }
+    else{
+        image.src = "pic_bulbon.gif";
+    }
+}
+
+
