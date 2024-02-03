baf8e0b091c9d55f6f05fe89693d8848329d5f37
“pinki <pinkijha1310@gmail.com>
2024-02-03 16:40:55 +0530

    hide the image when you click on image

--- 02-JS-fundamentals/Events/practice/practive-02/script.js
+++ 02-JS-fundamentals/Events/practice/practive-02/script.js
@@ -1,5 +1,5 @@
 
-//1 way
+/* //1 way
 document.getElementById("wada").onclick = function(){
     alert("wada pav is famous dish for mumbai!");
 }
@@ -19,6 +19,21 @@ document.getElementById("mdn").addEventListener("click", function(e){
 e.preventDefault();
 e.stopPropagation();
 console.log("Youe click On mdn");
-})
+}) */
 
 // in prev also use attachEvent & onEvent for old browsers
+
+
+/// hide the image when you click on image
+let image = document.getElementById("img-gallery").addEventListener("click", function(e){
+console.log(e.target.parentNode);
+image = e.target.tagName;
+console.log(image);
+if(image === 'IMG'){
+    console.log(image.id);
+    let removeIt = e.target.parentNode;
+    removeIt.remove();
+}
+
+
+})
