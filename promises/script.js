e2672f75a8b9259f892f64ffb5cc91299ad8a35f
“pinki <pinkijha1310@gmail.com>
2024-04-15 19:16:18 +0530

    create promise

---
+++ promises/script.js
@@ -0,0 +1,33 @@
+const cart = ["kurti", "lehnga", "saree", "tshirt"];
+
+const promise = createOrder(cart); //orderId
+promise.then(function(orderId){
+    console.log(orderId);
+})
+
+
+//producer
+function createOrder(cart){
+    const pr = new Promise(function (resolve, reject){
+        //vaidate card
+        if(!validateCart(cart)){
+            const err = new Error("The cart is not valid");
+            reject(err);
+        }
+
+        //logic for createId
+        const orderId = "123456";
+        if(orderId){
+            setTimeout(function(){
+                resolve(orderId); 
+            }, 5000)
+             
+        }
+    });
+
+    return pr;
+}
+
+function validateCart(cart){
+    return false;
+}
