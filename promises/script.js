71e2726ac62f30f82035dcd662e8eface7245ed9
“pinki <pinkijha1310@gmail.com>
2024-04-15 23:23:47 +0530

    create promise chaining

--- promises/script.js
+++ promises/script.js
@@ -1,11 +1,19 @@
 const cart = ["kurti", "lehnga", "saree", "tshirt"];
 
 const promise = createOrder(cart); //orderId
-promise.then(function(orderId){
+promise
+.then(function(orderId){
     console.log(orderId);
+    return orderId;
+})
+.then(function(orderId){
+    return proceedToPayment(orderId)
+})
+.then(function(paymentInfo){
+    console.log(paymentInfo);
 })
 .catch(function(err){
-    console.log(err);
+    console.log(err.message);
 })
 
 //producer
@@ -29,7 +37,12 @@ function createOrder(cart){
 
     return pr;
 }
-
+    
+function proceedToPayment(orderId){
+    return new Promise(function(resolve, reject){
+        resolve("payment successful");
+    })
+}
 function validateCart(cart){
-    return false;
+    return true;
 }
