8118459e92d3c97cfd111a11cbb96539bf4f2e7a
“pinki <pinkijha1310@gmail.com>
2024-04-16 16:26:37 +0530

    this inside a objects method

--- promises/namsteJavascript/thisKeyword/script.js
+++ promises/namsteJavascript/thisKeyword/script.js
@@ -6,6 +6,16 @@ console.log(this);
 function x(){
     console.log(this);
 }
+x();
 
 //this in strict mode - (this substitution)
 window.x();
+
+// this inside a objects method
+const obj = {
+    a:20,
+    x: function(){
+        console.log(this);
+    }
+}
+obj.x();
